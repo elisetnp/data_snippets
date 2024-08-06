f71b2d4cc951557cd5edc6c64bcbde5a9896721f
ET ✿ LacLac <tnpueh@gmail.com>
2024-08-06 21:55:17 +0200

    Update docker_basic_cmd.sh

--- docker_basic_cmd.sh
+++ docker_basic_cmd.sh
@@ -1,2 +1,7 @@
 # To perform a complete prune, including all unused data and volumes
 docker system prune -a --volumes -f
+
+# specify a project name explicitly using the -p flag when running docker-compose 
+docker-compose -p my_project_name up
+# run in detached mode
+docker-compose -p up -d

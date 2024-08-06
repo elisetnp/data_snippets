1d01e9f013dfd684d4d95d0138a22395552624b5
ET ✿ LacLac <tnpueh@gmail.com>
2024-08-06 15:40:54 +0200

    Create parse_git_branch.sh

---
+++ parse_git_branch.sh
@@ -0,0 +1,11 @@
+# parse git branch
+parse_git_branch() {
+    git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/[\1]/p'
+}
+COLOR_DEF='%f'
+COLOR_USR='%F{243}'
+COLOR_DIR='%F{30}'
+COLOR_GIT='%F{219}'
+NEWLINE=$'\n'
+setopt PROMPT_SUBST
+export PROMPT='${COLOR_DIR}%d ${COLOR_GIT}$(parse_git_branch)${COLOR_DEF}${NEWLINE}%% '
